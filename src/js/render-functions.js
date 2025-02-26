bfcbbf9f03c783c5bbd9bef062f4d95c3aabdb08
JaneBee24 <bublikkjenya@gmail.com>
2025-02-26 02:25:49 +0100

    2

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -20,3 +20,32 @@ export function renderGallery(images) {
 export function clearGallery() {
     gallery.innerHTML = "";
 }
+
+export function createGalleryMarkup(images) {
+    return images
+      .map(
+        ({
+          webformatURL,
+          largeImageURL,
+          tags,
+          likes,
+          views,
+          comments,
+          downloads,
+        }) => `
+        <li class="gallery-item">
+          <a class="gallery-link" href="${largeImageURL}">
+            <img class="gallery-image" src="${webformatURL}" alt="${tags}" />
+          </a>
+          <div class="image-info">
+            <p><strong>Likes:</strong> ${likes}</p>
+            <p><strong>Views:</strong> ${views}</p>
+            <p><strong>Comments:</strong> ${comments}</p>
+            <p><strong>Downloads:</strong> ${downloads}</p>
+          </div>
+        </li>
+      `
+      )
+      .join('');
+  }
+  
