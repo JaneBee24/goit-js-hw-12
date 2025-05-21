f7624f6ff16943d86c9c372a7bbbdfc74f50c12f
JaneBee24 <bublikkjenya@gmail.com>
2025-05-21 14:56:29 +0200

    1

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const API_KEY = 'твій_ключ';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export async function getImagesByQuery(query, page) {
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: 15,
+    page,
+  };
+
+  const response = await axios.get(BASE_URL, { params });
+  return response.data;
+}
---
+++ src/main.js
@@ -0,0 +1,88 @@
+import { getImagesByQuery } from './js/pixabay-api.js';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+  showLoadMoreButton,
+  hideLoadMoreButton,
+} from './js/render-functions.js';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+const loadMoreBtn = document.querySelector('.load-more');
+
+let query = '';
+let page = 1;
+let totalHits = 0;
+
+form.addEventListener('submit', async e => {
+  e.preventDefault();
+  query = e.target.elements['search-text'].value.trim();
+  if (!query) return;
+
+  clearGallery();
+  hideLoadMoreButton();
+  page = 1;
+
+  showLoader();
+  try {
+    const data = await getImagesByQuery(query, page);
+    totalHits = data.totalHits;
+
+    if (data.hits.length === 0) {
+      iziToast.info({ message: 'No images found.' });
+      return;
+    }
+
+    createGallery(data.hits);
+
+    if (totalHits > 15) {
+      showLoadMoreButton();
+    }
+  } catch (error) {
+    iziToast.error({ message: 'Something went wrong.' });
+  } finally {
+    hideLoader();
+  }
+});
+
+loadMoreBtn.addEventListener('click', async () => {
+  page += 1;
+  showLoader();
+  hideLoadMoreButton();
+
+  try {
+    const data = await getImagesByQuery(query, page);
+    createGallery(data.hits);
+
+    const totalPages = Math.ceil(totalHits / 15);
+    if (page >= totalPages) {
+      iziToast.info({
+        message: "We're sorry, but you've reached the end of search results.",
+      });
+      hideLoadMoreButton();
+    } else {
+      showLoadMoreButton();
+    }
+
+    scrollPage();
+  } catch (error) {
+    iziToast.error({ message: 'Failed to load more images.' });
+  } finally {
+    hideLoader();
+  }
+});
+
+function scrollPage() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery li')
+    .getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
